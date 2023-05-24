7976c01f0abb5f87c786bcd1fbc08e594fc1bbb3
Samy Francelet <samy.francelet@ik.me>
2023-05-25 01:56:08 +0200

    Je suis le type le plus fort de tous les temps git add ./git add ./git add ./

--- concurrent/main.cpp
+++ concurrent/main.cpp
@@ -11,119 +11,51 @@
 #include "path.hpp"
 #include "bnb.hpp"
 
-std::mutex bestPath;
-std::mutex tsp;
-
 std::stack<Path> paths;
 
-void thinking(Path *best, Matrix *pMatrix, bool *keepWaiting, int tid, int *threadStatus)
+void solve(Matrix *pMatrix, Path best)
 {
-    while (*keepWaiting) {
-        tsp.lock();
-        if (paths.empty()) {
-            threadStatus[tid] = 0; // I'm free !
-            // Check if all threads are done
-            int status = 0;
-            for (int i = 0; i < sizeof(threadStatus); i++) {
-                status += threadStatus[i];
-            }
+    std::chrono::steady_clock::time_point start, end;
+    EdgeMatrix edgeMatrix(pMatrix->order(), std::vector<int>(pMatrix->order(), 0));
+    Path root(pMatrix, edgeMatrix);
+    paths.push(root);
 
-            if (status == 0) {
-                *keepWaiting = false;
-            }
-            tsp.unlock();
-            continue;
-        }
-
-        threadStatus[tid] = 1; // I'm working hard here !
+    start = std::chrono::steady_clock::now();
+    while (!paths.empty()) {
         Path path = paths.top();
         paths.pop();
 
-        tsp.unlock();
-
-        if (path.is_route()) {
-            if (path.cost() <= best->cost()) {
-                bestPath.lock();
-                *best = path;
-                bestPath.unlock();
+        if (path.valid() && path.complete()) {
+            if (path.cost() < best.cost()) {
+                best = path;
             }
         }
 
-        if (!path.complete()) {
+        if (path.valid() && !path.complete()) {
             BnB bnb(path.edge_matrix());
-            Path leftChildPath(pMatrix, bnb.left());
-            Path rightChildPath(pMatrix, bnb.right());
+            Path left(pMatrix, bnb.left());
+            Path right(pMatrix, bnb.right());
 
-            double leftLowerBound = leftChildPath.lower_bound();
-            tsp.lock();
-            if (leftLowerBound <= best->cost()) {
-                // If the left path lower bound is less than the best path cost,
-                // then the left path is a candidate for the best path.
-                paths.push(leftChildPath);
+            if (left.lower_bound() <= best.cost()) {
+                paths.push(left);
             }
-            tsp.unlock();
 
-            double rightLowerBound = rightChildPath.lower_bound();
-            tsp.lock();
-            if (rightLowerBound <= best->cost()) {
-                // If the right path lower bound is less than the best path cost,
-                // then the right path is a candidate for the best path.
-                paths.push(rightChildPath);
+            if (right.lower_bound() <= best.cost()) {
+                paths.push(right);
             }
-            tsp.unlock();
         }
-    }
-}
-
-void start_tsp(Matrix *pMatrix) {
-    std::chrono::steady_clock::time_point begin, end;
-
-    int order = pMatrix->order();
-    EdgeMatrix edges(order, std::vector<int>(order, 0));
-
-    Path best(pMatrix, edges);
-    paths.push(best);
-    std::cout << "Initial best path: " << std::endl;
-    best.display();
-
-    unsigned int nCores = std::thread::hardware_concurrency();
-    unsigned int nThreads;
-
-    std::cout << "Number of possible threads: " << nCores << std::endl;
-    std::cout << "Please enter the number of threads to use: ";
-    std::cin >> nThreads;
-
-    if (nThreads > nCores) {
-        std::cout << "Number of threads requested is greater than the number of cores available. Using "
-                  << nCores << " threads." << std::endl;
-        nThreads = nCores;
-    }
-
-    std::thread threads[nThreads];
-    int threadStatus[1000];
-    for (int i = 0; i < 1000; i++) {
-        threadStatus[i] = i < nThreads ? 1 : 0; // 1 = running, 0 = no more work
-    }
-
-    bool keepWaiting = true;
-
-    begin = std::chrono::steady_clock::now();
-    for (int i = 0; i < nThreads; i++) {
-        threads[i] = std::thread(thinking, &best, pMatrix, &keepWaiting, i, threadStatus);
-    }
-
-    for (int i = 0; i < nThreads; i++) {
-        threads[i].join();
     }
     end = std::chrono::steady_clock::now();
 
-    std::chrono::duration<double> elapsedSeconds = end - begin;
+    std::cout << "Best path:" << std::endl;
     best.display();
-    std::cout << "Elapsed time: " << elapsedSeconds.count() << " seconds" << std::endl;
+
+    std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
     Matrix *matrix;
+    int nThreads = 1;
 
     if (argc == 2) {
         std::string tspFile(argv[1]);
@@ -141,14 +73,48 @@ int main(int argc, char* argv[]) {
                 matrix->sdistance(i, j) = defaultMatrix[i][j];
             }
         }
+    } else if (argc == 3) {
+        std::string tspFile(argv[1]);
+        matrix = TSPFile::matrix(tspFile);
+        nThreads = std::stoi(argv[2]);
     } else {
-        std::cout << "Usage: ./tsp <tsp file>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <tsp file> <n threads=1>" << std::endl;
         return 1;
     }
 
-    std::cout << "Matrix order: " << matrix->order() << std::endl;
-    matrix->display();
-    start_tsp(matrix);
+    //std::cout << "Matrix order: " << matrix->order() << std::endl;
+    //matrix->display();
+
+    // Generate initial path
+    EdgeMatrix edgeMatrix(matrix->order(), std::vector<int>(matrix->order(), -1));
+
+    // Generate initial path 0 -> 1 -> 2 -> ... -> n -> 0
+    // EdgeMatrix will look like this:
+    //      0    1    2    3    4
+    // 0    0    1    -1   -1   1
+    // 1    1    0    1    -1   -1
+    // 2    -1   1    0    1    -1
+    // 3    -1   -1   1    0    1
+    // 4    1    -1   -1   1    0
+
+    for (int i = 0; i < matrix->order(); i++) {
+        for (int j = 0; j < matrix->order(); j++) {
+            if (i == j) {
+                edgeMatrix[i][j] = 0;
+            } else if (i == j+1 || i+1 == j) {
+                edgeMatrix[i][j] = 1;
+            } else if ((i == 0 && j == matrix->order()-1) || (i == matrix->order()-1 && j == 0)) {
+                edgeMatrix[i][j] = 1;
+            } else {
+                edgeMatrix[i][j] = -1;
+            }
+        }
+    }
+
+    Path initialPath(matrix, edgeMatrix);
+    //initialPath.display();
+
+    solve(matrix, initialPath);
 
     return 0;
 }
